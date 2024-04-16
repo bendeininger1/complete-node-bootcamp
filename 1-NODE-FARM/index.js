75fe09ced93b31f06250f7544d2c9ca42d3591ed
Ben Deininger <bendeininger1@gmail.com>
2024-04-16 15:50:29 -0400

    add second read file[C

--- 1-NODE-FARM/index.js
+++ 1-NODE-FARM/index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 
 // Non-Blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err,data) => {
-    console.log(data);
-})
+fs.readFile('./txt/start.txt', 'utf-8', (err,data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err,data2) => {
+        console.log(data2);
+    });
+});
 console.log('Will read file!');
