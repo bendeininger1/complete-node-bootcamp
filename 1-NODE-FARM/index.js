6160c58b2d4bbe4ccf022af597b42ea9e8222278
Ben Deininger <bendeininger1@gmail.com>
2024-04-23 15:07:34 -0400

    added webserver

--- 1-NODE-FARM/index.js
+++ 1-NODE-FARM/index.js
@@ -1,9 +1,25 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-// Non-Blocking, asynchronous way
+/* // Non-Blocking, asynchronous way
 fs.readFile('./txt/start.txt', 'utf-8', (err,data1) => {
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err,data2) => {
         console.log(data2);
     });
 });
-console.log('Will read file!');
+console.log('Will read file!'); */
+
+const server = http.createServer((req,res) => {
+    const pathName = req.url ;
+
+    if(pathName === '/overview'){
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product'){
+        res.end('This is the PRODUCT');
+    };
+});
+
+server.listen(8000,'127.0.0.1', () => {
+    console.log('Listening on port 8000');
+});
