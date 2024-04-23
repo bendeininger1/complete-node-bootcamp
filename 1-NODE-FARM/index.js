33f5f6e6ce9580f9a11be9c81e5a31e2b31d8075
Ben Deininger <bendeininger1@gmail.com>
2024-04-23 15:11:36 -0400

    handle missing page

--- 1-NODE-FARM/index.js
+++ 1-NODE-FARM/index.js
@@ -17,7 +17,10 @@ const server = http.createServer((req,res) => {
         res.end('This is the OVERVIEW');
     } else if (pathName === '/product'){
         res.end('This is the PRODUCT');
-    };
+    } else {
+        res.writeHead(404);
+        res.end('Page not found!');
+    }
 });
 
 server.listen(8000,'127.0.0.1', () => {
