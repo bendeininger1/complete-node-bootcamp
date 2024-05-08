18d46e32aaba05b603366dfe7616092719199d50
Ben Deininger <bendeininger1@gmail.com>
2024-05-08 16:52:55 -0400

    add json API

--- 1-NODE-FARM/index.js
+++ 1-NODE-FARM/index.js
@@ -10,6 +10,9 @@ fs.readFile('./txt/start.txt', 'utf-8', (err,data1) => {
 });
 console.log('Will read file!'); */
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req,res) => {
     const pathName = req.url ;
 
@@ -17,8 +20,12 @@ const server = http.createServer((req,res) => {
         res.end('This is the OVERVIEW');
     } else if (pathName === '/product'){
         res.end('This is the PRODUCT');
-    } else if (pathName === '/api'){
-        res.end('API');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {
+          'Content-type': 'application/json'
+        });
+        res.end(data);
+    
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
