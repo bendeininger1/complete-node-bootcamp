70caf46b8ade73b3f3ca87bf5b5686822fc2c40b
Ben Deininger <bendeininger1@gmail.com>
2024-05-09 17:17:26 -0400

    added template function

--- 1-NODE-FARM/index.js
+++ 1-NODE-FARM/index.js
@@ -2,13 +2,19 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-/* // Non-Blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err,data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err,data2) => {
-        console.log(data2);
-    });
-});
-console.log('Will read file!'); */
+// SERVER
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
@@ -25,6 +31,9 @@ const server = http.createServer((req,res) => {
         res.writeHead(200, {
             'Content-type': 'text/html'
           });
+
+        const cardsHtml = dataObj.map( el => replaceTemplate(tempCard, el))
+
         res.end(tempOverview);
     } 
     
